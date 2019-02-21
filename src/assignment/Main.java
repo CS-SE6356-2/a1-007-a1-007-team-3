891460fb5f025f7b7e912b57b9fa8c801fe8cf09
yoseph <yosephwordofa1@gmail.com>
2019-02-21 15:41:59 -0600

    - Name 3

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -16,7 +16,7 @@ class NamePrinter {
 
         String[] names = {
                 "Name 1",
-                "Yoseph Wordofa1",
+                "Yoseph Wordofa",
                 "Name 3",
                 "Name 4",
                 "Name 5"};
