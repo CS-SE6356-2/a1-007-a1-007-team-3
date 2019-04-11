2d22d8630b7a4e395df155e6aafda5a0b64615e0
Clamibot <38234098+Clamibot@users.noreply.github.com>
2019-04-11 14:06:44 -0500

    Added Option To Play Another Round Without Exiting The Application
    I added in an option to play another round, which will cause the player ArrayList and the game deck to be reset upon the start of the next round. This way, the user doesn't have to exit the application to play another game.

--- Crazy Eights Game/Control.java
+++ Crazy Eights Game/Control.java
@@ -10,65 +10,88 @@ import java.util.*;
 
 /**
  * This class is the game engine. It takes care of all game logic.
+ *
  * @author Matthew Wethington, Martin Boerwinkle, Jonathan Guidry, Yoseph Wordofa
  */
-public class Control
-{    
+public class Control {
+
     public static void main(String args[])
     {
         Scanner in = new Scanner(System.in);
-        //Get the number of players playing the game
-        int numplayers = 0;
-        while(numplayers < 2)
+        int done = 0;
+        while (done == 0)
         {
-            System.out.println("How Many Players Will Be Playing Crazy Eights For This Session?");
-            numplayers = in.nextInt();
-            in.nextLine();//Set the read pointer to the next line and throw away the rest of the input that wasn't a number.
+            //Get the number of players playing the game
+            int numplayers = 0;
+            while (numplayers < 2)
+            {
+                System.out.println("How Many Players Will Be Playing Crazy Eights For This Session?");
+                numplayers = in.nextInt();
+                in.nextLine();//Set the read pointer to the next line and throw away the rest of the input that wasn't a number.
+
+                if (numplayers < 1)
+                    System.out.println("Invalid Number Of Players. Number Of Players Must Be In Range [2, 7].");
+                else if (numplayers > 7)
+                    System.out.println("Too Many Players. Maximum Allowed Is 7.");
+            }
+
+            //Construct the list of players.
+            Player players[] = new Player[numplayers];
+            for (int i = 0; i < numplayers; i++)
+                players[i] = new Player();
+
+            System.out.println("Constructing Deck And Discard Pile.");
+            //Declare and initialize the deck and discard pile
+            DiscardPile discardpile = new DiscardPile();
+            Deck gamedeck = new Deck(discardpile);
+            //Populate the deck with 52 cards, the standard deck size.
+            for (Rank r : Rank.values())
+                for (Suit s : Suit.values())
+                    gamedeck.InsertCard(new Card(s, r));
             
-            if(numplayers < 1)
-                System.out.println("Invalid Number Of Players. Number Of Players Must Be In Range [2, 7].");
-            else if(numplayers > 7)
-                System.out.println("Too Many Players. Maximum Allowed Is 7.");
-        }
-        
-        //Construct the list of players.
-        Player players[] = new Player[numplayers];
-        for(int i = 0; i < numplayers; i++)
-        {
-            players[i] = new Player();
-        }
-        
-        System.out.println("Constructing Deck And Discard Pile.");
-        //Declare and initialize the deck and discard pile
-        DiscardPile discardpile = new DiscardPile();
-        Deck gamedeck = new Deck(discardpile);
-        //Populate the deck with 52 cards, the standard deck size.
-        for(Rank r : Rank.values())
-        {
-            for(Suit s : Suit.values())
+            gamedeck.Shuffle();
+            discardpile.InsertCard(gamedeck.GetTopCard()); //Prime the discard pile
+            gamedeck.Deal(players);
+            System.out.println("Starting Game.");
+            //Start the game here (wait for the user interface to take shape)
+            UserInterface UI = new UserInterface(gamedeck, discardpile, in);
+            int playerIdx = 0;
+            int winner = -1;
+            while (winner == -1)//The game goes on until someone wins
             {
-                gamedeck.InsertCard(new Card(s, r));
+                System.out.println("Launching UI for player " + playerIdx + "!");
+                UI.SetCurrentPlayer(players[playerIdx]);//Player 1 gets to start the game
+                if (players[playerIdx].GetCardQuantity() == 0)
+                    winner = playerIdx;
+                playerIdx = (playerIdx + 1) % numplayers;
+            }
+            System.out.println("Winner is player " + winner);
+            System.out.println("Want To Play Another Round? (Respond Using Y For Yes Or N For No)");
+            int responsegotten = 0;
+            
+            while (responsegotten == 0)
+            {
+                String response = in.nextLine();//Make response not case sensitive
+                switch (response)
+                {
+                    case "N":
+                        done = 1;
+                        responsegotten = 1;
+                        break;
+                    case "n":
+                        done = 1;
+                        responsegotten = 1;
+                        break;
+                    case "Y":
+                        responsegotten = 1;
+                        break;
+                    case "y":
+                        responsegotten = 1;
+                        break;
+                    default: System.out.println("Invalid Response. Please Try Again.");
+                }
             }
         }
-        gamedeck.Shuffle();
-        discardpile.InsertCard(gamedeck.GetTopCard()); //Prime the discard pile
-        gamedeck.Deal(players);
-        System.out.println("Starting Game.");
-        //Start the game here (wait for the user interface to take shape)
-        UserInterface UI = new UserInterface(gamedeck, discardpile, in);
-        int playerIdx = 0;
-        int winner = -1;
-        while(winner == -1)//The game goes on until someone wins
-        {
-            System.out.println("Launching UI for player "+playerIdx+"!");
-            UI.SetCurrentPlayer(players[playerIdx]);//Player 1 gets to start the game
-            if(players[playerIdx].GetCardQuantity() == 0)
-            {
-                winner = playerIdx;
-            }
-            playerIdx = (playerIdx+1)%numplayers;
-        }
-        System.out.println("Winner is player "+winner);
         System.out.println("Thanks For Playing!");
     }
 }
